815b3920e1862f29b94a891ebbca3a778558bfce
MauroBos <152307343+MauroBos@users.noreply.github.com>
2024-03-21 12:32:51 -0300

    Delete dist directory

--- dist/routes/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_1 = require("../controllers/user");
-const router = (0, express_1.Router)();
-router.post('/', user_1.signUp);
-router.post('/login', user_1.signIn);
-router.get('/list', user_1.userList);
-exports.default = router;
